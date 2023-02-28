168068bcf936c6c2119501f676477f4a39e0e601
Tapash Mollick <tapashmollick@mplgaming.com>
2023-02-28 16:16:15 +0530

    added code

---
+++ AnalyticsFrameWork/AnalyticsFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  AnalyticsFrameWork.h
+//  AnalyticsFrameWork
+//
+//  Created by Tapash Mollick on 28/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnalyticsFrameWork.
+FOUNDATION_EXPORT double AnalyticsFrameWorkVersionNumber;
+
+//! Project version string for AnalyticsFrameWork.
+FOUNDATION_EXPORT const unsigned char AnalyticsFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnalyticsFrameWork/PublicHeader.h>
+
+
